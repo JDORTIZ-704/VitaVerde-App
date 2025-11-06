0d1f4eecf4eff6d041084a08ea6b1f97c2409086
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-11-06 10:15:50 -0500

    Correcciones y actualizaciones para DAO

--- app/src/main/java/com/ucompensar/project_store/database/UsersDAO.kt
+++ app/src/main/java/com/ucompensar/project_store/database/UsersDAO.kt
@@ -7,80 +7,96 @@ import com.ucompensar.project_store.utils.PasswordHelper
 
 class UsersDAO (context: Context) {
 
-    //Instancia del DatabaseHelper -> DB
+    // Instance of DatabaseHelper -> DB-USERS
 
     private val dbHelper = DataBaseHelper(context)
 
-    //función de registro
+    //************************//
+    //                        //
+    // Registration function  //
+    //                        //
+    //************************//
 
-    fun registerUser(users: Users): Boolean {
-        //Abrir modo escritura DB
+    /*
+    * 1. Open the database in write mode
+    * 2. Encrypt password
+    * 3. Statement of updated values
+    */
 
+    fun registerUser (users: Users): Boolean {
         val db = dbHelper.writableDatabase
-        //Encriptar contraseña antes de guardarla
         val passwordHashed = PasswordHelper.hashPassword(users.password)
-        //contenedor Clave - Valor
         val values = ContentValues().apply {
             put(DataBaseHelper.COLUMN_NAME, users.name)
             put(DataBaseHelper.COLUMN_EMAIL, users.email)
             put(DataBaseHelper.COLUMN_PASSWORD, passwordHashed)
         }
 
-        //insert los valores en la tabla
-        //si retorna -1 hubo error al insertar, sino, return id
+        // Insert values into the table
+        // Return -1 if there is an error
 
         val resultInsert = db.insert(DataBaseHelper.TABLE_USERS, null, values)
         db.close()
         return resultInsert != -1L
     }
-    // Validar Login
-    fun validateLogin(email: String, password: String): Users? {
 
-        //Obtener usuario BD x email.
+    //************************//
+    //                        //
+    // Login validation       //
+    //                        //
+    //************************//
 
-        val users = getUserByEmail(email)?: return false
-        // verificar si el password coincide
-        val passwordMatch = PasswordHelper.hashPassword(password)
+    fun validateLogin (email: String, password: String): Boolean {
 
+        // Retrieve user from database
+        // If user is null, return false
+        // If user is not null, compare passwords
 
-
+        val user = getUserByEmail(email)?: return false
+        val passwordMatch = PasswordHelper.hashPassword(user.password)
+        return user.password == passwordMatch
     }
 
-    // Obtener el usuario DB
+    // Function to retrieve users from the database
 
-    fun getUserByEmail(email: String): Users? {
-        // abrir base de datos en modo lectura
-        var user: Users? = null
+    fun getUserByEmail (email: String): Users? {
         val dbReadUser = dbHelper.readableDatabase
+        var user: Users? = null
 
-        // Generar Query para obtener el usuario
+        // Query to get the user by email
 
         val queryGetUser = """  
             SELECT * FROM ${DataBaseHelper.TABLE_USERS}
             WHERE ${DataBaseHelper.COLUMN_EMAIL} = ?
         """
-        // Ejecutar query
+
         val cursor = dbReadUser.rawQuery(queryGetUser, arrayOf(email))
 
-        //recorrer cursor
         if (cursor.moveToFirst()) {
             val id = cursor.getInt(cursor.getColumnIndexOrThrow(DataBaseHelper.COLUMN_ID))
             val name = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.COLUMN_NAME))
             val email = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.COLUMN_EMAIL))
             val password = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.COLUMN_PASSWORD))
             user = Users(id, name, email, password)
-
-
         }
-
         cursor.close()
         dbReadUser.close()
         return user
-
     }
-    // Validar el email (si existe)
 
-    fun validateEmail(email: String): Boolean {
+    //***********************************************************//
+    //                                                           //
+    // Validation - If the email already exists in the database  //
+    //                                                           //
+    //***********************************************************//
+
+    /*
+    * 1. Open the database in read mode
+    * 2. Query to get the user by email
+    * 3. Check if the cursor has a count greater than 0
+    */
+
+    fun validateEmail (email: String): Boolean {
         val dbReadUser = dbHelper.readableDatabase
         val queryGetUser = """  
             SELECT * FROM ${DataBaseHelper.TABLE_USERS}
@@ -93,9 +109,19 @@ class UsersDAO (context: Context) {
         return emailExists
 
     }
-    // Eliminar usuario
 
-    fun deleteUser(email: String): Boolean {
+    //************************//
+    //                        //
+    // Delete user            //
+    //                        //
+    //************************//
+
+    /*
+    * 1. Open the database in write mode
+    * 2. Query to delete a user via email
+    */
+
+    fun deleteUser (email: String): Boolean {
         val dbWriteUser = dbHelper.writableDatabase
         val rowsDeleted = dbWriteUser.delete(
             DataBaseHelper.TABLE_USERS,
@@ -106,10 +132,19 @@ class UsersDAO (context: Context) {
         return rowsDeleted > 0
     }
 
+    //************************//
+    //                        //
+    // Update password        //
+    //                        //
+    //************************//
 
-    // Actualizar contraseña
+    /*
+    * 1. Open the database in write mode
+    * 2. Query to update password via email
+    * 3. Encrypt password
+    */
 
-    fun updateUserPassword(email: String, newPassword: String): Boolean {
+    fun updateUserPassword (email: String, newPassword: String): Boolean {
 
         val dbWriteUser = dbHelper.writableDatabase
         val passwordHashed = PasswordHelper.hashPassword(newPassword)
@@ -127,9 +162,18 @@ class UsersDAO (context: Context) {
         return resultUpdate > 0
     }
 
-    // Actualizar nombre
+    //************************//
+    //                        //
+    // Update name            //
+    //                        //
+    //************************//
 
-    fun updateNameUser(email: String, newName: String): Boolean {
+    /*
+    * 1. Open the database in write mode
+    * 2. Query to update user via email
+    */
+
+    fun updateNameUser (email: String, newName: String): Boolean {
         val dbWriteUser = dbHelper.writableDatabase
         val valuesNewName = ContentValues().apply {
             put(DataBaseHelper.COLUMN_NAME, newName)
@@ -145,8 +189,26 @@ class UsersDAO (context: Context) {
         return resultUpdate > 0
     }
 
-    // Actualizar email
+    //************************//
+    //                        //
+    // Update Email           //
+    //                        //
+    //************************//
 
 
+    fun updateEmail (email: String, newEmail: String): Boolean {
+        val dbWriteUser = dbHelper.writableDatabase
+        val valuesNewEmail = ContentValues().apply {
+            put(DataBaseHelper.COLUMN_EMAIL, newEmail)
+        }
 
+        val resultUpdate = dbWriteUser.update(
+            DataBaseHelper.TABLE_USERS,
+            valuesNewEmail,
+            "${DataBaseHelper.COLUMN_EMAIL} = ?",
+            arrayOf(email)
+        )
+        dbWriteUser.close()
+        return resultUpdate > 0
+    }
 }
