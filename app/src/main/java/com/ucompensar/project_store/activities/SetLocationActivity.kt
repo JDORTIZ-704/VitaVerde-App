fe98c03146400de38d04305dc34516af0628f940
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-11-09 00:14:03 -0500

    Creación del activity setlocation para primer ingreso

--- app/src/main/java/com/ucompensar/project_store/activities/SetLocationActivity.kt
+++ app/src/main/java/com/ucompensar/project_store/activities/SetLocationActivity.kt
@@ -1,4 +1,30 @@
 package com.ucompensar.project_store.activities
 
-class SetLocationActivity {
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ucompensar.project_store.R
+
+class SetLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_set_location)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val optionsSpinner = arrayOf("Option 1", "Option 2", "Option 3")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, optionsSpinner)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+
+
+
+
+
+    }
 }
