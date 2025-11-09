e1dde910b2ea28c31324f4023f9eeda593fe5118
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-11-09 10:36:09 -0500

    Integración squash de feature/google

--- app/src/main/java/com/ucompensar/project_store/activities/SetLocationActivity.kt
+++ app/src/main/java/com/ucompensar/project_store/activities/SetLocationActivity.kt
@@ -1,37 +1,16 @@
-package com.ucompensar.project_store.activities
-
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.Spinner
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.ucompensar.project_store.R
-import org.w3c.dom.Text
 
 class SetLocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_set_location)
 
-        val spinner = findViewById<Spinner>(R.id.spinner)
-        val optionsSpinner = arrayOf("Option 1", "Option 2", "Option 3")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, optionsSpinner)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-
-
-        val btn = findViewById<Button>(R.id.btn_setlocation_confirmar)
 
         btn.setOnClickListener {
-            val selectedOption = spinner.selectedItem.toString()
-            val textView : TextView? = findViewById(R.id.textView5)
-            if (textView != null) {
-                textView.text = selectedOption
             }
         }
     }
