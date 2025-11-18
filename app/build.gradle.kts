aa342fac7c87a6a4a9ffabce3b505af62b35a6c5
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-11-17 19:50:14 -0500

    Guardar cambios antes de cambiar de rama

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,18 @@ dependencies {
     implementation(libs.googleid.v111)
     implementation(libs.kotlinx.coroutines.android)
 
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    // Also add the dependencies for the Credential Manager libraries and specify their versions
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
